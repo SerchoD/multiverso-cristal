d7feab780583c430431dacba72070b95c5ecc7c6
Sergio Díaz <diaz.sergio@protonmail.com>
2023-04-12 23:38:35 -0300

    eliminando el Loader que estaba sin usarse

--- src/pages/TestePage/TestPage.js
+++ src/pages/TestePage/TestPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PageContainer from '../../components/PageContainer/PageContainer';
-import Loader from '../../components/Loader/Loader';
 
 const TestPage = () => {
 	return (
@@ -8,7 +7,6 @@ const TestPage = () => {
 			<h1 className='flex justify-center items-center h-full w-full border-2  absolute top-0 left-0 z-50**'>
 				/TEST
 			</h1>
-			{/* <Loader /> */}
 		</PageContainer>
 	);
 };
