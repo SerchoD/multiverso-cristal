54cc415b3463297212e07cb70a043847674e7609
Sergio Díaz <diaz.sergio@protonmail.com>
2023-05-05 01:57:44 -0300

    finalizado selector del topbar desktop

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,43 +1,76 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import useStyles from './useStyles';
 import { PATHS } from '../../constants/paths';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Text from '../Text/Text';
+import './Topbar.css';
 
 const TOPBAR_ELEMENT = [
-	// { text: 'Home', route: PATHS.BASE_URL },
-	{ text: 'Gemoterapia', route: PATHS.GEMSTONE_THERAPY },
-	{ text: 'Cirugía Astral', route: PATHS.ASTRAL_SURGERY },
+	// { name: 'Home', route: PATHS.BASE_URL },
+	{ name: 'Gemoterapia', route: PATHS.GEMSTONE_THERAPY },
+	{ name: 'Cirugía Astral', route: PATHS.ASTRAL_SURGERY },
 ];
 
 const Topbar = () => {
 	const navigate = useNavigate();
 	const { pathname: currentRoute } = useLocation();
+	const [indicatorPosition, setIndicatorPosition] = useState({});
 
 	const s = useStyles({ currentRoute });
 
-	const handleRouteClick = ({ route }) => {
-		navigate(route);
+	const handleRouteClick = (item, element) => {
+		const itemData = element.getBoundingClientRect();
+
+		navigate(item?.route);
+
+		setIndicatorPosition({
+			width: `${itemData.width - 10}px`,
+			left: `${itemData.left + 5}px`,
+		});
 	};
+
+	useEffect(() => {
+		const allItems = document.querySelector('.topbar-menu');
+
+		const currentItemName = TOPBAR_ELEMENT.find(
+			(element) => element.route === currentRoute
+		)?.name;
+
+		const currentItem = Array.from(allItems?.childNodes).find(
+			(e) => e.textContent === currentItemName
+		);
+
+		const itemData = currentItem.getBoundingClientRect();
+
+		if (allItems) {
+			setIndicatorPosition({
+				width: `${itemData.width - 10}px`,
+				left: `${itemData.left + 5}px`,
+			});
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
 	return (
 		<div className={s?.main_container}>
-			{TOPBAR_ELEMENT.map((item, index) => {
-				let isActive = false;
-				if (item.route === currentRoute) isActive = true;
+			<ul className='topbar-menu'>
+				{TOPBAR_ELEMENT.map((item) => {
+					let isActive = false;
+					if (item.route === currentRoute) isActive = true;
 
-				return (
-					<div
-						key={index}
-						className={`${s?.element_container} ${isActive && s?.active_route}`}
-						onClick={() => handleRouteClick({ route: item.route })}
-					>
-						<Text text={item.text} textType={2} />
-						{index !== TOPBAR_ELEMENT.length - 1 && (
-							<div className='w-[20px] '>{/* Separator */}</div>
-						)}
-					</div>
-				);
-			})}
+					return (
+						<div key={item.name}>
+							<li
+								className={`${s?.element_container} ${isActive && s?.active_route}`}
+								onClick={(e) => handleRouteClick(item, e.target)}
+							>
+								<Text text={item.name} textType={2} className={'pb-1 px-1 -mx-3'} />
+							</li>
+						</div>
+					);
+				})}
+			</ul>
+			<div className='topbar-indicator' style={indicatorPosition} />
 		</div>
 	);
 };
--- src/components/Topbar/useStyles.js
+++ src/components/Topbar/useStyles.js
@@ -4,15 +4,15 @@ const useStyles = ({ currentRoute }) => {
     		flex justify-center items-center   
    			| bg-gradient-to-r from-purple-300/[50%] via-green-300/[30%] to-red-300/[50%] |
 				shadow-[0_0px_10px_10px_rgba(0,0,0,.3)]`,
-		element_container: `flex cursor-pointer 
+		element_container: `flex cursor-pointer 				
 				transition-all
 				text-shadow-1
 				text-amber-300/[75%]
 				hover:text-amber-200
 				hover:scale-[105%]
-				hover:text-shadow-2
+				hover:text-shadow-6
 		  `,
-		active_route: `text-amber-200 scale-[105%] text-shadow-2`,
+		active_route: `text-amber-200 scale-[105%] text-shadow-6`,
 	};
 
 	return styles;
