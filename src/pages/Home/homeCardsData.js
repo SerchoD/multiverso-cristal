dc757cf39a7e13dfc7d350036d6c4c47b834e11e
Sergio Díaz <diaz.sergio@protonmail.com>
2023-05-31 22:50:58 -0300

    fixin routes in new path object

--- src/pages/Home/homeCardsData.js
+++ src/pages/Home/homeCardsData.js
@@ -12,14 +12,14 @@ export const CARD_DATA = [
 		description:
 			'La Gemoterapia es una herramienta para Desbloquear y Armonizar el cuerpo energético. Los Chakras son centros energéticos de nuestro cuerpo, a traves de los cuales fluye nuestra energía vital. A veces, por dolor, estrés, tristesa, malos pensamientos, se pueden bloquear, desarmonizar, o distorcionar uno o varios Chakras, impidiendo que nuestra energía fluya correctamente. Provocando mal humor, cansancio, baja energía y con el tiempo síntomas más notables físicos, psíquicos y emocionales. Dependiendo la naturaleza de la causa y la experiencia del individuo, diferentes Chakras se pueden alterar de distinta manera en correlacción a la intencidad y tipo de conflicto. Con Gemoterapia, se trabaja directamente con la energía de los cristales naturales de la tierra, activándolos sobre el cuerpo energético del individuo a tratar. Al ser una técnica energética, se puede practicar de manera presencial así como también a distancia, viendo resultados en todos los casos.',
 		image: 'bg-img-2',
-		route: PATHS.GEMSTONE_THERAPY,
+		route: PATHS.GEMSTONE_THERAPY.route,
 	},
 	{
 		title: 'Cirugía Astral',
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat null',
 		image: 'bg-img-3',
-		route: PATHS.ASTRAL_SURGERY,
+		route: PATHS.ASTRAL_SURGERY.route,
 	},
 	{
 		title: 'Registros Akáshicos',
