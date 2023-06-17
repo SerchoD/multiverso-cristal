8ddf8b09d3894dc643ecd729dfe23757d173ea6a
Sergio Díaz <diaz.sergio@protonmail.com>
2023-06-16 21:10:45 -0300

    commit parcial

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import { randomHexadecimal as rnd } from '../../utils/random';
+import Slider from 'infinite-react-carousel';
+
+const Carousel = () => {
+	return (
+		<div className='w-full h-[500px] border-2 my-5'>
+			{/* <Slider
+				className='border-2 border-red-600 h-auto w-auto'
+				dots
+				slidesPerRow={3}
+			></Slider> */}
+		</div>
+	);
+};
+
+export default Carousel;
--- src/pages/GemstoneTherapy/GemstoneTherapy.js
+++ src/pages/GemstoneTherapy/GemstoneTherapy.js
@@ -3,12 +3,14 @@ import PageContainer from '../../components/PageContainer/PageContainer';
 import FaQAccordion from '../../components/FAQAccordion/FAQAccordion';
 import { GEMSTONE_FAQ } from './gemstone_FAQ';
 import FormToEmail from '../../components/FormToEmail/FormToEmail';
+import Carousel from '../../components/Carousel/Carousel';
 
 const GemstoneTherapy = () => {
 	return (
 		<PageContainer>
 			<h1 className='mt-10'>Página de Gemoterapia</h1>
 
+			<Carousel />
 			<FaQAccordion FAQ={GEMSTONE_FAQ} className={''} />
 			<FormToEmail className='mt-10' />
 		</PageContainer>
