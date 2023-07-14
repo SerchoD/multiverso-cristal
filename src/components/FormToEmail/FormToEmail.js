b614e649bb7f541e4beab251a52dd0d8b1b37601
Sergio Díaz <diaz.sergio@protonmail.com>
2023-07-14 17:38:10 -0300

    Corregidos warnings

--- src/components/FormToEmail/FormToEmail.js
+++ src/components/FormToEmail/FormToEmail.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import AnimatedContainer from '../AnimatedContainer/AnimatedContainer';
 import { useLocation } from 'react-router-dom';
 import useStyles from './useStyles';
